6d4adb26c22e79a71997c704f5bc29c892305772
faybian123 <55282176+faybian123@users.noreply.github.com>
2020-01-13 15:16:24 +0000

    newer version of the modularity code

---
+++ Modularity-master/U16 Modularity/ConsoleApp1/Calculator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Calculator
+    {
+        //This is a calculator class and below are the member functions:
+
+        //Addition Function
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        //Subtraction Function
+        //Basic validation added to ensure that we subtract the smaller number from the larger number
+        public double Subtract(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+            return num2 - num1;
+        }
+
+        //Multiplication Function
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        //Division Function
+        public double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Modularity-master/U16 Modularity/ConsoleApp1/U16_Modular_Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class U16_Modular_Calculator
+    {
+                {
+            Calculator MyCalc = new Calculator();
+
+
+        Console.WriteLine("###################################");
+            Console.WriteLine("######## C# Calculator ############");
+            Console.WriteLine("###################################");
+
+
+            bool calcLoop = true;
+            while (calcLoop == true)
+            {
+                Console.Write("\nEnter your first number:  ");
+                double inputOne = Convert.ToDouble(Console.Readline());
+
+        Console.Write("\nEnter your second number:  ");
+                double inputTwo = Convert.ToDouble(Console.Readline());
+
+        Console.WriteLine("\nMenu");
+                Console.WriteLine("Enter 1 for addition");
+                Console.WriteLine("Enter 2 for subtraction");
+                Console.WriteLine("Enter 3 for multiplication");
+                Console.WriteLine("Enter 4 for division");
+                Console.WriteLine("");
+
+                int opChoice = Convert.ToInt32(Console.ReadLine());
+                switch (opChoice)
+                {
+                    case 1:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Add(inputOne, inputTwo).ToString());
+                        break;
+
+                    case 2:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Subtract(inputOne, inputTwo).ToString());
+                        break;
+
+                    case 3:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Multiply(inputOne, inputTwo).ToString());
+                        break;
+
+                    case 4:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Divide(inputOne, inputTwo).ToString());
+                        break;
+                    default:
+                        Console.WriteLine("\nYou should enter a number from the menu.");
+                        break;
+    }
+}
